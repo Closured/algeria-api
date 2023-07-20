1ec604f9b9a4463d5a295f8e0edc7b0d00e74841
Closured <kamal.ennour.chehami@gmail.com>
2023-07-20 18:03:15 +0100

    switched to koa-cors to allow cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import Koa from "koa";
 import { router } from "./routes/router.js";
 import json from "koa-json";
+import cors from "koa-cors";
 import dotenv from "dotenv";
 export const api = new Koa();
 dotenv.config();
 
-api.use(async (ctx) => {
-    ctx.set("Access-Control-Allow-Origin", "*");
-    ctx.set("Access-Control-Allow-Methods", "GET");
-    ctx.set("Access-Control-Allow-Headers", "Content-Type");
-});
+api.use(cors());
 
 api.use(json()).use(router.routes()).use(router.allowedMethods());
 

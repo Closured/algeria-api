ac6bb45bcb06c2ad2ec287c7eaceb47ea038baf9
Closured <kamal.ennour.chehami@gmail.com>
2023-07-20 18:00:51 +0100

    cors allowed only for GET method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import dotenv from "dotenv";
 export const api = new Koa();
 dotenv.config();
 
+api.use(async (ctx) => {
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set("Access-Control-Allow-Methods", "GET");
+    ctx.set("Access-Control-Allow-Headers", "Content-Type");
+});
+
 api.use(json()).use(router.routes()).use(router.allowedMethods());
 
 api.listen(process.env.PORT, () => console.log("Listening on Port", process.env.PORT));
